e61616140753a09e2c4556a07f08603e85d4a1f3
JK <jrileykruger@gmail.com>
2022-12-08 13:12:37 -0800

    removed sign in with google button

--- components/account/AccountNavSignInButton.js
+++ components/account/AccountNavSignInButton.js
@@ -3,21 +3,11 @@ import Link from "next/link";
 
 export default function AccountNavSignInButton() {
   return (
-    <>
-      <button
-        className="text-dark-theme-6 font-bold text-sm flex justify-center items-center my-5 mr-2 px-2 bg-dark-theme-blue rounded"
-        onClick={() => {
-          signIn("google", { callbackUrl: window.location.href });
-        }}
-      >
-        SignInWithGoogle
-      </button>
-      <Link
-        href="/api/auth/signin"
-        className="text-dark-theme-6 font-bold text-sm flex justify-center items-center my-5 mr-2 px-2 bg-dark-theme-blue rounded"
-      >
-        Sign In
-      </Link>
-    </>
+    <Link
+      href="/api/auth/signin"
+      className="text-dark-theme-6 font-bold text-sm flex justify-center items-center my-5 mr-2 px-2 bg-dark-theme-blue rounded"
+    >
+      Sign In
+    </Link>
   );
 }
